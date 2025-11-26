8773a0771b2f288498dd0efa03e5f510dda7c0cf
kirankhant_11357 <keshavofficial57@gmail.com>
2025-11-26 13:55:18 +0530

    second

--- furniture-frontend/src/api/auth.js
+++ furniture-frontend/src/api/auth.js
@@ -4,4 +4,13 @@ export const login = async(username,password) => {
     const res = await api.post('token/',{username,password});
     localStorage.setItem('access_token',res.data.access);
     localStorage.setItem('refresh_token',res.data.refresh);
-}
+    return res.data;
+};
+
+export const refreshToken = async () => {
+    const refresh = localStorage.getItem('refresh_token');
+    if(!refresh) throw new Error('no refresh token');
+    const res = await api.post('token/refresh/',{refresh});
+    localStorage.setItem('access_token',res.data.access);
+    return res.data;
+};
